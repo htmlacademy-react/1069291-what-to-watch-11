76567898f505ef883a89750047756760a81a6594
Mikhail Nopin <nopinmike@gmail.com>
2022-11-25 17:21:46 +0300

    Начинаем программировать

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -8,6 +8,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App name="The Grand Budapest Hotel" genre="Drama" releaseYear="2014" />
   </React.StrictMode>,
 );
